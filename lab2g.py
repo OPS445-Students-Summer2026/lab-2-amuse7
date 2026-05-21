e86adebf2d351b99650e00c6aa593ec24cab10ee
Abdirashid Muse <amuse7@myseneca.ca>
2026-05-21 11:04:21 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Abdirashid Muse
+# Author ID: amuse7
+# Date Created: 2026/05/21
+
+import sys
+
+if len(sys.argv) > 1:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
