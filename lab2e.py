8fc8c0b78503b83e77fae0761bdeed56418945f1
Abdirashid Muse <amuse7@myseneca.ca>
2026-05-21 10:56:02 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Abdirashid Muse
+# Author ID: amuse7
+# Date Created: 2026/05/21
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
